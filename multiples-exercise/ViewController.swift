2d64dcb5f7a0212a4ede5ec1a5b189cd9f7ed70e
ejwalters <ejwalters24@gmail.com>
2015-12-21 22:59:52 -0500

    Added files for multiples-exercise

--- multiples-exercise/ViewController.swift
+++ multiples-exercise/ViewController.swift
@@ -9,16 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Properties
+    var multipleNumber = 0
+    var currentNumber = 0
+    var newTotal = 0
+    let maxNumber = 200
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var multipleTxt: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var directionsLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var sumLabel: UILabel!
+    
+    @IBAction func onAddButtonPressed(sender: UIButton){
+        newTotal = multipleNumber + currentNumber
+        
+        updateSumLabel()
+        
+        if isMaxReached(){
+            logoImg.hidden = false
+            multipleTxt.hidden = false
+            playButton.hidden = false
+            
+            directionsLabel.hidden = true
+            addButton.hidden = true
+            sumLabel.hidden = true
+            multipleTxt.text = ""
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton){
+        
+        if(multipleTxt.text != nil && multipleTxt.text != ""){
+            logoImg.hidden = true
+            multipleTxt.hidden = true
+            playButton.hidden = true
+            
+            directionsLabel.hidden = false
+            addButton.hidden = false
+            
+            multipleNumber = Int(multipleTxt.text!)!
+        }
     }
+    
+    func updateSumLabel(){
+        
+        directionsLabel.hidden = true
+        sumLabel.hidden = false
+        
+        sumLabel.text = "\(currentNumber) + \(multipleNumber) = \(newTotal)"
+        currentNumber = newTotal
+        
+    }
+    
+    func isMaxReached() -> Bool{
+        if newTotal >= maxNumber{
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    
 
 
 }
